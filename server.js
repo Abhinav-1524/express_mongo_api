2d2d335c43b38eca3b6df00b3c7b8e2fab118219
Abhinav <karriabhinav15@gmail.com>
2023-10-04 17:40:01 +0530

    added create and fetch operations

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const productdata = require('./models/productmodel')
 dotenv.config()
 const port = 3000
 
 
+app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
@@ -23,3 +25,36 @@ mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser:true })
     .catch((error) => {
         console.error("Error connecting to MongoDB:", error);
     });
+
+
+app.post('/product',async (req,res) => {
+    try{
+        const product = await productdata.create(req.body)
+        res.status(200).json(product)
+    }catch(error){
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/product',async (req,res) => {
+    try {
+        const product = await productdata.find({});
+        res.status(200).json(product)
+    }catch (e){
+        console.log(e.message)
+        res.status(500).json({message:e.message})
+    }
+})
+
+app.get('/product/:id',async (req,res) =>{
+    try {
+        const {id} = req.params;
+        const product = await productdata.findById(id);
+        res.status(200).json(product)
+    }catch (e){
+        console.log(e.message)
+        res.status(500).json({message:e.message})
+    }
+})
+
